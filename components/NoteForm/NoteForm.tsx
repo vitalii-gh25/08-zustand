acbbfc0e18e85607cf59a89fedf041f0f4bedee0
Vitalii_gh <a.g.vitalii2022@gmail.com>
2025-12-23 20:06:20 +0200

    rename import in NoteForm

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -4,7 +4,7 @@
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { useMutation } from '@tanstack/react-query';
-import { useNoteStore } from '@/lib/store/store';
+import { useNoteStore } from '@/lib/store/noteStore';
 import css from './NoteForm.module.css';
 
 type CreateNoteDto = {
