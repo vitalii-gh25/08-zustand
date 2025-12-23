e5476a7477ad7c1bc3796c30c3cfad8c6fcce31d
Vitalii_gh <a.g.vitalii2022@gmail.com>
2025-12-23 19:32:02 +0200

    new

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -4,7 +4,7 @@
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { useMutation } from '@tanstack/react-query';
-import { useNoteStore } from '@/lib/Store';
+import { useNoteStore } from '@/lib/store';
 import css from './NoteForm.module.css';
 
 type CreateNoteDto = {
